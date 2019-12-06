634659d5032d8656fbd93758d686db352bab28f5
Jeff Tian <jie.tian@unicareer.com>
2019-12-06 18:28:40 +0800

    bug: proto 3

---
+++ test/health.spec.js
@@ -0,0 +1,11 @@
+const { unlinkSync, existsSync } = require('fs');
+const { execSync } = require('child_process');
+const assert = require('assert');
+
+module.exports = [
+    (async function testCLI() {
+        execSync('./cli.js ./test/source ./test/target health.proto');
+        assert.ok(existsSync('./test/target/X.ts'));
+        unlinkSync('./test/target/X.ts');
+    })()
+];
